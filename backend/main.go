936a7088f3a181db3f21630a6b1b899e278d30f7
Magic Lea <barygame@gmail.com>
2018-04-18 02:25:11 +0800

    [BE] Crawler stores item info into mongo db

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Magiclea/poe-stash-search/backend/services/crawler"
 	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
 )
 
-func init() {
-	crawler := crawler.New("http://api.pathofexile.com")
+var (
+	mongoURL   = flag.String("mongo_url", "localhost", "mongo DB URL")
+	poeBaseURL = flag.String("poe_base_url", "http://api.pathofexile.com", "poe base URL")
+
+	mgoSession = &mgo.Session{}
+)
+
+func main() {
+	flag.Parse()
+
+	setupMongo(*mongoURL)
+	startCrawler()
+
+	r := gin.Default()
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	r.Run()
+}
+
+func setupMongo(url string) {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+	mgoSession = session
+	session.SetMode(mgo.Monotonic, true)
+}
+
+func startCrawler() {
+	crawler := crawler.New(*poeBaseURL, mgoSession)
 	go func() {
 		// TODO: retrieve lastID from mysql db
 		lastID := "2811-4457-4108-4795-1510"
@@ -16,15 +48,4 @@ func init() {
 			fmt.Println("crawler.FollowStashStream failed:", err)
 		}
 	}()
-}
-
-func main() {
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-
-	r.Run()
 }
--- backend/services/crawler/crawler.go
+++ backend/services/crawler/crawler.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Crawler is a crawler
@@ -14,18 +17,22 @@ type Crawler struct {
 	// mongo db
 	// TODO:
 	apiBaseURL string
+	mgo        *mgo.Session
 }
 
 // New returns a crawler
-func New(baseURL string) *Crawler {
+func New(baseURL string, mgo *mgo.Session) *Crawler {
 	return &Crawler{
 		apiBaseURL: baseURL,
+		mgo:        mgo,
 	}
 }
 
 // FollowStashStream follows public stash change stream, and stores changes into db
 func (c *Crawler) FollowStashStream(lastID string) error {
 	nextID := lastID
+	conn := c.mgo.Copy()
+	defer conn.Close()
 	// FIXME: it is a demo, and just do 10 times
 	for i := 0; i < 10; i++ {
 		url := fmt.Sprintf("%s/public-stash-tabs/?id=%s", c.apiBaseURL, nextID)
@@ -42,6 +49,30 @@ func (c *Crawler) FollowStashStream(lastID string) error {
 		if err := json.Unmarshal(body, &resMap); err != nil {
 			return err
 		}
+
+		conn.DB("poe-stash-search").C("StashChange").Insert(
+			map[string]interface{}{
+				"change_id":      nextID,
+				"next_change_id": resMap["next_change_id"],
+			})
+		bulk := conn.DB("poe-stash-search").C("Item").Bulk()
+		bulk.Unordered()
+		if resMap["stashes"] != nil {
+			for _, stash := range resMap["stashes"].([]interface{}) {
+				stashMap := stash.(map[string]interface{})
+				if stashMap["items"] != nil {
+					for _, item := range stashMap["items"].([]interface{}) {
+						itemMap := item.(map[string]interface{})
+						selector := bson.M{"id": itemMap["id"]}
+						update := itemMap
+						update["stash_id"] = stashMap["id"]
+						update["stash_type"] = stashMap["stashType"]
+						bulk.Upsert(selector, update)
+					}
+				}
+			}
+		}
+		bulk.Run()
 
 		fmt.Println("next id:", resMap["next_change_id"])
 		nextID = resMap["next_change_id"].(string)
